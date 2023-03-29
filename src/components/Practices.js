511ab8a7b8f24c1a25f6ed3b066ad105898f3a0e
Matt Ayers <73685857+mattayersnz@users.noreply.github.com>
2023-03-29 21:53:10 +1300

    card expanding

--- src/components/Practices.js
+++ src/components/Practices.js
@@ -1,14 +1,68 @@
 import React from 'react';
 import styled from 'styled-components';
+import { BrowserRouter as Router, Routes, Route, Link, useParams } from 'react-router-dom';
 
 export default function Practices() {
   return (
-    <PracticesContainer>
-      <PracticesHeader>Listening Practices</PracticesHeader>
-      <PracticesGrid />
-    </PracticesContainer>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <PracticesContainer>
+            <PracticesHeader>Listening Practices</PracticesHeader>
+            <PracticesGrid />
+          </PracticesContainer>
+        } />
+        <Route path="/card/:title" element={
+          <>
+            <BackgroundDiv />
+            <CardPage />
+          </>
+        } />
+      </Routes>
+    </Router>
   );
 }
+
+const CardPage = () => {
+  const { title } = useParams();
+  const card = cardContentData.find(c => c.title === title);
+
+  return (
+    <CardPageContainer>
+      <CardHeader>{card.title}</CardHeader>
+      <CardContentStyle>{card.content}</CardContentStyle>
+      <CloseButton onClick={() => window.history.back()}>Close</CloseButton>
+    </CardPageContainer>
+  );
+};
+
+const BackgroundDiv = styled.div`
+  background-color: white;
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+`;
+
+const CardPageContainer = styled.div`
+  background-color: white;
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+  padding: 10%;
+`;
+
+const CloseButton = styled.button`
+  background-color: white;
+  border: 1px solid #212121;
+  margin-top: 24px;
+  cursor: pointer;
+`;
 
 const PracticesContainer = styled.div`
   margin-top: -5%;
@@ -24,20 +78,32 @@ const PracticesContainer = styled.div`
 
 const PracticesHeader = styled.div`
   font-family: 'Yeseva One', cursive;
-  font-size: 36px;
-  padding-bottom: 2rem;
-  @media only screen and (max-width: 900px) {
-    font-size: 24px;
-  }
+  font-size: 32px;
+  padding-bottom: 24px;
+`;
+
+const CardHeader = styled.div`
+  font-family: 'Yeseva One', cursive;
+  font-size: 32px;
+  padding-bottom: 24px;
+  color: #212121;
+`;
+
+const CardContentStyle = styled.div`
+  font-family: 'Inter', sans-serif;
+  font-size: 12px;
+  color: #212121;
 `;
 
 const PracticesGrid = () => {
   return (
     <GridContainer>
       {cardContentData.map((cardContent, index) => (
-        <Card key={index}>
-          <CardContent title={cardContent.title} content={cardContent.content} />
-        </Card>
+        <Link to={`/card/${cardContent.title}`} key={index}>
+          <Card>
+            <CardContent title={cardContent.title} content={cardContent.content} />
+          </Card>
+        </Link>
       ))}
     </GridContainer>
   );
@@ -54,8 +120,11 @@ const GridContainer = styled.div`
 const Card = styled.div`
   border: 0.25px solid #DCDCDC;
   padding: 0.5rem 2rem 2rem 2rem;
-  background-color: #FAFAFA;
+  background-color: #FDFDFD;
   transition: box-shadow 0.3s ease, transform 0.3s ease, background-color 0.3s ease;
+  font-family: 'Inter', sans-serif;
+  text-decoration: none;
+  color: #212121;
 
   &:hover {
     box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
@@ -64,7 +133,6 @@ const Card = styled.div`
     cursor: pointer;
   }
 `;
-
 
 const CardContent = ({ title, content }) => {
   return (
@@ -85,3 +153,4 @@ const cardContentData = [
   { title: "Exploring Ideas", content: "Explore ideas with someone in a way that also helps you explore their experience" },
   { title: "Engaging Groups", content: "Participate in a group in a way that explores how they form meaning together" }
 ];
+
